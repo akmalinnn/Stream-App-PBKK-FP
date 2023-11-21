e0695f7e6c19919a61adaec45da75dbc04d65fe1
Vermillion8 <5025211101@mhs.its.ac.id>
2023-11-21 22:49:28 +0700

    updated search functionalities

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,8 +1,8 @@
 <div class="relative border border-white bg-black p-1 text-white">
     <x-bi-search class="absolute m-1 h-5 w-5" />
-    <input wire:model.debounce.500ms="search" type="text" placeholder="Titles, people, genres" class="ml-8 bg-black text-white placeholder-gray-500 focus:outline-none">
+    <input type="text" wire:model.live.debounce.500ms="search" placeholder="Titles, people, genres" class="ml-8 bg-black text-white placeholder-gray-500 focus:outline-none">
 
-    @if (strlen($search >= 3))
+    @if (strlen($search) >= 3)
     <div class="absolute mt-2 -ml-2 w-64 bg-gray-700 p-2 text-sm">
         @if ($searchResults->count() > 0 )
         <ul>
